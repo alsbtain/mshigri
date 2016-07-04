b70f2a0de4e950bc1c34e78ddbd29356960d8704
alsbtain <info@alsbtain.net>
2016-07-04 05:59:23 -0700

    update

--- www/settings.js
+++ www/settings.js
@@ -2,6 +2,11 @@ var siteURL = 'http://www.mshigri.com/';
 
 var shareURL = siteURL + 'media_redirect.php';
 
+var internalMenu = { 
+    title: ["الأخبار", "المقالات", "مكتبة الصور", "مكتبة الميديا", "مجلد التحميلات"],
+    url: ["news.html", "news.html?articles=1", "gallery.html", "media.html", "downloads.html"]
+};
+
 var externalMenu = { 
     title: ["الموقع الرسمي"],
     url: ["http://www.mshigri.com/"]
